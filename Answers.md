939e7b35564b3c040d126b0cc67570cfa3509ef9
Denis <denisnovikov@garant.ru>
2015-03-26 12:17:16 +0300

    Formatted answers
     Print answers in Markdown format

---
+++ Answers.md
@@ -0,0 +1,16 @@
+#Project Euler Answers
+
+##Solved by
+
+Denis Novikov
+
+##URL
+
+https://github.com/Denisss025/Euler-Project-Solutions-Golang
+
+##Answers
+
+| Problem | Result |
+|:-------:| ------ |
+|   001   | 233168 |
+|   002   | 4613732 |
--- src/main.go
+++ src/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
-    "euler"
-    _ "euler/solutions"
-    "fmt"
+	"euler"
+	_ "euler/solutions"
+	"fmt"
 )
 
+const title = `#Project Euler Answers
+
+##Solved by
+
+Denis Novikov
+
+##URL
+
+https://github.com/Denisss025/Euler-Project-Solutions-Golang
+
+##Answers
+
+| Problem | Result |
+|:-------:| ------ |`
+
 func main() {
-    euler.RunSolutions(func(name string, solution euler.SolutionFunc) {
-        if result, err := solution(); err != nil {
-            fmt.Println("Problem", name, "finished with error:", err)
-        } else {
-            fmt.Println("Problem", name + ":", result)
-        }
-    })
+	fmt.Print(title)
+
+	fmt.Println()
+	euler.RunSolutions(func(name string, solution euler.SolutionFunc) {
+		fmt.Print("|   ", name, "   | ")
+		if result, err := solution(); err != nil {
+			fmt.Print(err)
+		} else {
+			fmt.Print(result)
+		}
+		fmt.Println(" |")
+	})
 }
