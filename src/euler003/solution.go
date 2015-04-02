cabbc211afefac3a0b822ac1edfaba6aa425ff3f
Denis <denisnovikov@garant.ru>
2015-04-02 15:11:24 +0300

    [003] Check for zero
     Add check of zero value in function maxPrimeFactor while shifting
     val to the right (division by 2).
    
     Add better for-loop.

--- src/euler003/solution.go
+++ src/euler003/solution.go
@@ -1,21 +1,16 @@
 package euler003
 
-import (
-	"euler"
-)
+import "euler"
 
 func maxPrimeFactor(val uint64) uint64 {
-	var divisor uint64 = 3
-
-	for val&0x1 == 0 {
+	for val != 0 && val&0x1 == 0 {
 		val >>= 1
 	}
 
-	for divisor < val {
+	for divisor := uint64(3); divisor < val; divisor += 2 {
 		for val%divisor == 0 {
 			val /= divisor
 		}
-		divisor += 2
 	}
 
 	return val
