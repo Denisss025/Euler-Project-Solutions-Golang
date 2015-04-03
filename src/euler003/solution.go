b4fe905f0f1e693a16f8b883fcf2116d00648996
Denis <denisnovikov@garant.ru>
2015-04-03 13:32:26 +0300

    [003] Optimize performance

--- src/euler003/solution.go
+++ src/euler003/solution.go
@@ -1,18 +1,29 @@
 package euler003
 
-import "euler"
+import (
+	"euler"
+	"euler/math"
+)
 
 func maxPrimeFactor(val uint64) uint64 {
-	for val != 0 && val&0x1 == 0 {
+	for val > 2 && val&0x1 == 0 {
 		val >>= 1
 	}
 
-	for divisor := uint64(3); divisor < val; divisor += 2 {
-		for val%divisor == 0 {
+	for val > 3 && val%3 == 0 {
+		val /= 3
+	}
+
+	for divisor := uint64(5); divisor < math.Sqrt(val); divisor += 4 {
+		for val > divisor && val%divisor == 0 {
+			val /= divisor
+		}
+
+		divisor += 2
+		for val > divisor && val%divisor == 0 {
 			val /= divisor
 		}
 	}
-
 	return val
 }
 
