7d7dbd2b6163196a93479321e6635697a5bdd5e5
Denis Novikov <denisnovikov@garant.ru>
2015-10-08 12:17:53 +0300

    [006] Faster solution
     * Add comments
    
     * Another formula

--- src/euler006/solution.go
+++ src/euler006/solution.go
@@ -1,43 +1,25 @@
 package euler006
 
-import (
-	"euler"
-	"euler/math"
-	"fmt"
-)
+import "euler"
 
-func diff(max uint64) (result uint64, err error) {
-	if max < 3 {
-		return (0x2 & max) * max, nil
-	}
+func diff(n uint64) (uint64, error) {
+	s := n
+	n++
+	s *= n
+	// sum = n*(n+1)/2
+	s >>= 1
+	n <<= 1
+	n--
+	n *= s
+	// n = sum * (2*(n + 1) - 1) / 3 =
+	//   = n(n+1)(2n + 1)/6 =
+	//   = (2*n^3 + 3*n^2 + n)/6 =
+	//   = (n^3)/3 + (n^2)/2 + n/6
+	n /= 3
+	// s = square(sum)
+	s *= s
+	return s - n, nil
 
-	if math.MaxU64/max < max {
-		return 0, fmt.Errorf("Integer overflow")
-	}
-
-	if math.MaxU64/(max*max) < (max * max) {
-		return 0, fmt.Errorf("Integer overflow")
-	}
-
-	a, b := (max+1)>>2, (max-1)>>2
-	a *= (b << 1) + 1
-
-	result, b = a*(max-1), (max-3)>>2
-	for a = 0; a < b; a++ {
-		result -= ((a*a + a) << 3) + 2
-	}
-	a <<= 2
-	a += 2
-	result -= b * a
-	if (max-1)&0x2 == 0 {
-		result -= (b + 1) * a
-	}
-
-	a = 2 + 3*max
-	result *= a
-	a *= (1 - (a & 0x1)) * max * max
-	result += a >> 3
-	return
 }
 
 func Solution() (uint64, error) {
