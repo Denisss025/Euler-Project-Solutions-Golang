7c2490982c9e8e57bf14ea1498764a062689af29
Denis Novikov <denisnovikov@garant.ru>
2015-12-15 10:34:50 +0300

    [002] Remove goroutines
     Replace goroutines and channels with FibGenerator structure (class).

--- src/euler002/solution.go
+++ src/euler002/solution.go
@@ -2,34 +2,36 @@ package euler002
 
 import "euler"
 
-func fibGen(fib chan uint64) {
-	fib <- 0
+// FibGenerator is a Fibonacci sequence generator.
+type FibGenerator struct{ n, p uint64 }
 
-	prev, curr := uint64(0), uint64(1)
+// NewFibGenerator creates a new Fibonacci sequence generator.
+func NewFibGenerator(n, p uint64) *FibGenerator { return &FibGenerator{n, p} }
 
-	for {
-		fib <- curr
-		prev, curr = curr, curr+prev
-	}
+// Next calculates and returns the next Fibonacci number.
+func (f *FibGenerator) Next() uint64 {
+	f.p, f.n = f.n, f.n+f.p
+	return f.p
 }
 
+// sumEvenFib calculates the sum of the even-valued terms of the Fibonacci
+// sequence.
 func sumEvenFib(limit uint64) (sum uint64, err error) {
-	fib := make(chan uint64, 10)
-	go fibGen(fib)
-
-	for fibNum := <-fib; fibNum < limit; fibNum = <-fib {
-		if fibNum&0x1 == 0x0 {
-			sum += fibNum
+	fib := NewFibGenerator(0, 1) // create new Fibonacci sequence generator
+	for n := uint64(0); n < limit; n = fib.Next() {
+		if n&1 == 0 {  // check if n is even
+			sum += n
 		}
 	}
-
 	return
 }
 
+// Solution solves the Project Euler Problem #2 and returns a solution.
 func Solution() (uint64, error) {
-	return sumEvenFib(4000000)
+	return sumEvenFib(4e6)
 }
 
+// init registers the Solution function.
 func init() {
 	euler.RegisterSolution("002", Solution)
 }
